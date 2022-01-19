4c6659959e78d64d0994b4ef2ba5b08ad85033bd
kubanuch <kurbanbekatan@gmail.com>
2022-01-20 02:41:35 +0300

    исправил ошибку

--- app/src/main/java/com/example/todomanager06/fragment/CreateTaskFragment.java
+++ app/src/main/java/com/example/todomanager06/fragment/CreateTaskFragment.java
@@ -1,21 +1,32 @@
 package com.example.todomanager06.fragment;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.DatePicker;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.navigation.Navigation;
 
 import com.example.todomanager06.R;
 import com.example.todomanager06.databinding.FragmentCreateTaskBinding;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
-public class CreateTaskFragment extends Fragment {
+import java.util.Calendar;
+
+public class CreateTaskFragment extends BottomSheetDialogFragment implements DatePickerDialog.OnDateSetListener {
     FragmentCreateTaskBinding binding;
+    private int startYear;
+    private int startMonth;
+    private int startDay;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,6 +48,18 @@ public class CreateTaskFragment extends Fragment {
                 sendText();
             }
         });
+        binding.chooseDateTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePickerDialog();
+            }
+        });
+        binding.chooseRepeatTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showRepeatDialog();
+            }
+        });
     }
 
     private void sendText() {
@@ -48,5 +71,68 @@ public class CreateTaskFragment extends Fragment {
             bundle.putString("key", text);
             Navigation.findNavController(requireView()).navigate(R.id.homeFragment, bundle);
         }
+    }
+
+    private void showDatePickerDialog() {
+        Calendar calendar = Calendar.getInstance();
+        startYear = calendar.get(Calendar.YEAR);
+        startMonth = calendar.get(Calendar.MONTH);
+        startDay = calendar.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(requireContext(), this, startYear, startMonth, startDay);
+        datePickerDialog.show();
+    }
+
+    private void showRepeatDialog() {
+        LayoutInflater inflater = LayoutInflater.from(requireContext());
+        @SuppressLint("InflateParams") View view = inflater.inflate(R.layout.repeat_dialog, null);
+        Dialog alertDialog = new Dialog(requireContext());
+        alertDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        alertDialog.setContentView(view);
+        alertDialog.show();
+        RadioButton never = alertDialog.findViewById(R.id.never_radioBtn);
+        RadioButton everyDay = alertDialog.findViewById(R.id.Every_day_btn);
+        RadioButton everyWeer = alertDialog.findViewById(R.id.Every_week_btn);
+        RadioButton everyMonth = alertDialog.findViewById(R.id.Every_month_btn);
+        RadioButton everyYear = alertDialog.findViewById(R.id.Every_year_btn);
+        never.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.chooseRepeatTv.setText("Never");
+                alertDialog.dismiss();
+                }
+        });
+        everyDay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.chooseRepeatTv.setText("Every day");
+                alertDialog.dismiss();
+            }
+        });
+        everyWeer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.chooseRepeatTv.setText("Every week");
+                alertDialog.dismiss();
+            }
+        });
+        everyMonth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.chooseRepeatTv.setText("Every month");
+                alertDialog.dismiss();
+            }
+        });
+        everyYear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.chooseRepeatTv.setText("Every year");
+                alertDialog.dismiss();
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        binding.chooseDateTv.setText("" + day + "." + month + 1 + "." + year);
     }
 }
